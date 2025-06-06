a029bedeecae7685063c3ea59a8d3f8708334a1a
8-prime <hoffmeister.stefan99@gmail.com>
2025-06-06 19:23:08 +0200

    basic version working and remove old code

--- src/pages/SolariTime.tsx
+++ src/pages/SolariTime.tsx
@@ -1,90 +1,175 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
+type SolariProps = {
+    flaps: string[] // all the available values to be displayed
+    current: number // index of the current value
+}
 
+const animationDuration = 200;
 
-export default function SolariTime() {
-    const current = "A"
-    const next = "B"
+function SolariTop({ flaps, current }: SolariProps) {
+    const currentLetter = flaps[current];
+    const nextLetter = flaps[(current + 1) % flaps.length];
 
-    const [lowerStage, setLowerStage] = useState(0);
-    const [upperStage, setUpperStage] = useState(0);
+    const [isTransitioning, setIsTransitioning] = useState(false);
+    const [stage, setStage] = useState(0);
 
-    const flip = () => {
-        setLowerStage(1)
-        const flipTimer2 = setTimeout(() => {
-            setUpperStage(1)
-        }, 100);
-        return () => {
-            clearTimeout(flipTimer2);
-        };
-    }
+    const isFirstRender = useRef(true);
 
-
-    const reset = () => {
-        setLowerStage(0);
-        setUpperStage(0);
+    const transform = (stage: number) => {
+        switch (stage) {
+            case 0:
+                return 'rotateX(-90deg)';
+            case 1:
+                return 'rotateX(0deg)';
+            default:
+                return 'rotateX(-90deg)';
+        }
     };
 
-    const getUpperTransform = (stage: number) => {
+    useEffect(() => {
+        console.log("running use effect: " + isFirstRender.current);
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+        console.log("running use effect: " + isFirstRender.current);
+
+
+        setIsTransitioning(false);
+        setStage(0);
+        const timeout = setTimeout(() => {
+            setIsTransitioning(true);
+            setStage(1);
+        }, animationDuration * 2);
+
+        return () => {
+            clearTimeout(timeout);
+        }
+    }, [flaps, current])
+
+    return (
+        <div className="grid text-white text-5xl">
+            <div
+                className="col-start-1 row-start-1 w-32 h-16 overflow-hidden transition-transform duration-100 ease-in-out bg-neutral-700 rounded-t-lg"
+            >
+                <div className="text-8xl font-bold text-white leading-none flex items-start justify-center h-32 ">
+                    {currentLetter}
+                </div>
+            </div>
+            <div
+                className={`col-start-1 row-start-1 w-32 h-16 overflow-hidden bg-neutral-700 rounded-t-lg z-10 ${isTransitioning ? `transition-transform duration-${animationDuration} ease-in-out` : ''}`}
+                style={{
+                    transform: transform(stage),
+                    transformStyle: 'preserve-3d',
+                    transformOrigin: 'bottom center'
+                }}
+            >
+                <div className="text-8xl font-bold text-white leading-none flex items-start justify-center h-32 ">
+                    {nextLetter}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function SolariBottom({ flaps, current }: SolariProps) {
+    const currentLetter = flaps[current];
+    const nextLetter = flaps[(current + 1) % flaps.length];
+
+    const [isTransitioning, setIsTransitioning] = useState(false);
+    const [stage, setStage] = useState(0);
+
+    const isFirstRender = useRef(true);
+
+    useEffect(() => {
+        if (isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+
+        setIsTransitioning(false);
+        setStage(0);
+        const interval = setInterval(() => {
+            setIsTransitioning(true);
+            setStage(1);
+        }, animationDuration);
+
+        return () => {
+            clearInterval(interval);
+        }
+    }, [flaps, current])
+
+    const transform = (stage: number) => {
         switch (stage) {
             case 0:
                 return 'rotateX(0deg)';
             case 1:
                 return 'rotateX(90deg)';
             default:
-                return 'rotateX(0deg)';
-        }
-    };
-
-    const getLowerTransform = (stage: number) => {
-        switch (stage) {
-            case 0:
-                return 'rotateX(-90deg)';
-            case 1:
-                return 'rotateX(0deg)';
-            default:
                 return 'rotateX(-90deg)';
         }
     };
 
     return (
+        <div className="grid text-white text-5xl">
+            <div
+                className="col-start-1 row-start-1 w-32 h-16 overflow-hidden ease-in-out bg-neutral-700 rounded-b-lg"
+            >
+                <div className="text-8xl font-bold text-white leading-none flex items-start justify-center h-32 -translate-y-1/2">
+                    {nextLetter}
+                </div>
+            </div>
+            <div
+                className={`col-start-1 row-start-1 w-32 h-16 overflow-hidden bg-neutral-700 rounded-b-lg z-10 ${isTransitioning ? `transition-transform duration-${animationDuration} ease-in-out` : ''}`}
+                style={{
+                    transform: transform(stage),
+                    transformStyle: 'preserve-3d',
+                    transformOrigin: 'top center'
+                }}
+            >
+                <div className="text-8xl font-bold text-white leading-none flex items-start justify-center -translate-y-1/2 h-32 ">
+                    {currentLetter}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default function SolariTime() {
+
+    const alphabet = "0123456789".split("");
+    const [current, setCurrent] = useState(0);
+
+    const reset = () => {
+        setCurrent(0);
+    }
+
+
+    const [paused, setPaused] = useState(false);
+
+    // useEffect(() => {
+    //     if (paused) return;
+    //     const inter = setInterval(() => {
+    //         setCurrent((c) => (c + 1) % alphabet.length);
+    //     }, 1000);
+    //     return () => {
+    //         clearInterval(inter);
+    //     }
+    // }, [paused])
+
+
+    return (
         // <HingeRotateDiv />
         <div className="min-h-screen flex flex-col  items-center justify-evenly text-white bg-neutral-950">
             <div className="flex flex-col">
-                <div
-                    className="col-start-1 row-start-1 w-32 h-16 overflow-hidden transition-transform duration-100 ease-in-out bg-neutral-700 rounded-b-lg z-10"
-                    style={{
-                        transform: getLowerTransform(upperStage),
-                        transformStyle: 'preserve-3d',
-                        transformOrigin: 'top center',
-                    }}
-                >
-                    {/* The letter itself */}
-                    <div className="text-8xl font-bold text-white leading-none flex items-start justify-center h-32 ">
-                        B
-                    </div>
-                </div>
-                {/* Container for the half letter with 3D transform */}
-                <div
-                    className="col-start-1 row-start-1 w-32 h-16 overflow-hidden transition-transform duration-100 ease-in-out bg-neutral-700 rounded-b-lg z-10"
-                    style={{
-                        transform: getUpperTransform(lowerStage),
-                        transformStyle: 'preserve-3d',
-                        transformOrigin: 'bottom center'
-                    }}
-                >
-                    {/* The letter itself */}
-                    <div className="text-8xl font-bold text-white leading-none flex items-start justify-center h-32 -translate-y-1/2">
-                        A
-                    </div>
-                </div>
-
-
-                {/* Reference line to show the "cut" */}
-                <div className="absolute top-0 left-0 w-full h-px bg-red-500 opacity-50"></div>
+                <SolariTop current={current} flaps={alphabet} />
+                <hr className="h-0.5 m-0 p-0 border-t-0 bg-neutral-950" />
+                <SolariBottom current={current} flaps={alphabet} />
             </div>
-            <button onClick={() => reset()}>Reset</button>
-            <button onClick={() => flip()}>Flip</button>
+            <button className="bg-neutral-900 px-4 py-2 rounded-lg shadow-lg" onClick={() => reset()}>Reset</button>
+            <button className="bg-neutral-900 px-4 py-2 rounded-lg shadow-lg" onClick={() => setPaused(!paused)}>Pause</button>
         </div >
     );
 }
